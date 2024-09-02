3cc0731ac934edfce71785c5e285ba9cea825d1a
Gabriel Duarte <gabriel.duarte18@outlook.es>
2024-09-02 17:05:36 -0400

    Country component

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -5,28 +5,28 @@ function CountryCard() {
   const countries = useFetchCountries();
 
   return (
-    <>
-      <section className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10'>
-        {countries.map((country) => {
-          const { name, population, region, capital, flags } = country;
-          return (
-            <div key={nanoid()} className='country-card'>
-              <img src={flags.png} alt={`Flag of ${name.common}`} className='country-flag' />
-              <h3 className='country-name'>{name.common}</h3>
-              <h4 className='country-detail'>
-                Population: <span>{population.toLocaleString()}</span>
+    <section className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-10 p-4'>
+      {countries.map((country) => {
+        const { name, population, region, capital, flags } = country;
+        return (
+          <article key={nanoid()} className='bg-[#2b3743] rounded-md overflow-hidden shadow-lg w-[350px]'>
+            <img src={flags.png} alt={`Flag of ${name.common}`} className='w-full h-[250px] object-cover' />
+            <div className='bg-[#2b3743] py-4 px-5'>
+              <h3 className='text-lg font-semibold text-white mb-3'>{name.common}</h3>
+              <h4 className='text-sm text-gray-200 mb-1'>
+                Population: <span className='font-light'>{population.toLocaleString()}</span>
               </h4>
-              <h4 className='country-detail'>
-                Region: <span>{region}</span>
+              <h4 className='text-sm text-gray-200 mb-1'>
+                Region: <span className='font-light'>{region}</span>
               </h4>
-              <h4 className='country-detail'>
-                Capital: <span>{capital ? capital[0] : 'N/A'}</span>
+              <h4 className='text-sm text-gray-200'>
+                Capital: <span className='font-light'>{capital ? capital[0] : 'N/A'}</span>
               </h4>
             </div>
-          );
-        })}
-      </section>
-    </>
+          </article>
+        );
+      })}
+    </section>
   );
 }
 
