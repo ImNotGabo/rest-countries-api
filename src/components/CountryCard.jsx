77e0845a19e14abebafb455146e69b5e64e5515f
Gabriel Duarte <gabriel.duarte18@outlook.es>
2024-09-14 11:55:24 -0400

    Card UI fixed

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -5,14 +5,14 @@ function CountryCard() {
   const countries = useFetchCountries();
 
   return (
-    <section className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-10 p-4'>
+    <section className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-20 gap-y-[90px] p-4 max-w-screen-2xl mx-auto'>
       {countries.map((country) => {
         const { name, population, region, capital, flags } = country;
         return (
-          <article key={nanoid()} className='bg-[#2b3743] rounded-md overflow-hidden shadow-lg w-[350px]'>
-            <img src={flags.png} alt={`Flag of ${name.common}`} className='w-full h-[250px] object-cover' />
-            <div className='bg-[#2b3743] py-4 px-5'>
-              <h3 className='text-lg font-semibold text-white mb-3'>{name.common}</h3>
+          <article key={nanoid()} className='bg-[#2b3743] rounded-md overflow-hidden shadow-lg h-[400px]'>
+            <img src={flags?.png} alt={`Flag of ${name.common}`} className='w-full h-[220px] object-fill' />
+            <div className='bg-[#2b3743] py-5 px-6'>
+              <h3 className='text-lg font-bold text-white mb-3'>{name.common}</h3>
               <h4 className='text-sm text-gray-200 mb-1'>
                 Population: <span className='font-light'>{population.toLocaleString()}</span>
               </h4>
